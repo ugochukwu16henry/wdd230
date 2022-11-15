e772446e2de6039105a6a7911b519ba2963c7ad6
ugochukwu16henry <ugo21002@byui.edu>
2022-11-15 14:33:31 +0100

    update

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -3,7 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 //url for the weather
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Guayaquil&appid=32187ba698e081994209f85c896de7dc&units=imperial";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Guayaquil&appid=6ea909c44918475a8f20085878e2613b&units=metric";
 
 async function apiFetch() {
     try {
