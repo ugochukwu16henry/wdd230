72d59394ab598bc56d71b873cb530b7cfbf62e35
ugochukwu16henry <ugo21002@byui.edu>
2022-11-15 13:47:26 +0100

    update

---
+++ chamber/scripts/weatherapi.js
@@ -0,0 +1,44 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption'); //url for the weather
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Owerri&appid=32187ba698e081994209f85c896de7dc&units=imperial";
+
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            // displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    windSpeed.textContent = weatherData.wind.speed;
+    humidity.textContent = weatherData.main.humidity;
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+
+    const captionDesc = document.createElement('figcaption');
+    captionDesc.textContent = desc;
+
+    figureElem.appendChild(weatherIcon);
+    figureElem.appendChild(captionDesc);
+}
+
+apiFetch();
