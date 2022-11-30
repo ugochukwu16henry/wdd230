f38614de3657dc594fe5ef4cb99007f1e53bc47d
ugochukwu16henry <ugo21002@byui.edu>
2022-11-30 16:43:47 +0100

    update

---
+++ finalproject/scripts/main.js
@@ -0,0 +1,11 @@
+// get the local time to add in the footer
+const dt = new Date();
+
+//get the year from the date variable
+const year = dt.getFullYear();
+
+//DOM to manipulate the span tag at the footer
+document.querySelector("#year").textContent = year;
+
+//last modified date 
+document.querySelector("#lmodified").textContent = document.lastModified;
